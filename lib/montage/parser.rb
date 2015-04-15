b78367dadcec30864f14525fe194348cf350747b
Zach <zackify@gmail.com>
2015-04-15 13:56:12 -0700

    converted to spaces?

--- lib/montage/parser.rb
+++ lib/montage/parser.rb
@@ -5,34 +5,34 @@ module Montage
   class Parser
     attr_reader :parse, :query_operator, :column_name, :clause, :condition_set
 
-  	OPERATOR_MAP = {
+    OPERATOR_MAP = {
       "!=" => "__not",
-			">=" => "__gte",
-			"<=" => "__lte",
-			"="  => "",
+      ">=" => "__gte",
+      "<=" => "__lte",
+      "="  => "",
       ">"  => "__gt",
       "<"  => "__lt",
       "not in" => "__notin",
       "in" => "__in"
     }
 
-  	def initialize(clause=nil)
-  		if(clause)
-  			@clause = clause
-   			@column_name = get_column_name
-   			@query_operator = get_operator
-   			@query_parts = get_query_parts
+    def initialize(clause=nil)
+      if(clause)
+        @clause = clause
+        @column_name = get_column_name
+        @query_operator = get_operator
+        @query_parts = get_query_parts
         @condition_set = parse_query_value
 
-   			@parse = parse_string_clause
-   		end
-  	end
+        @parse = parse_string_clause
+      end
+    end
 
-  	def get_column_name
-  		@clause.downcase.split(' ')[0]
-  	end
+    def get_column_name
+      @clause.downcase.split(' ')[0]
+    end
 
-  	def get_operator
+    def get_operator
       operator = OPERATOR_MAP.find { |key, value| @clause.downcase.include?(' ' + key + ' ') }
       if !operator 
         raise QueryError, "The operator you have used is not a valid Montage query operator"
@@ -40,20 +40,20 @@ module Montage
       operator
     end
 
-  	def get_query_parts
-  		query_parts = false
-  		OPERATOR_MAP.each do |key, value|
-			  if @clause.downcase.include? key
-			  	query_parts = @clause.downcase.gsub(key, value)
-			  	break
-			  end
-			end
-			return false unless query_parts
-			query_parts.split(' ')
-  	end
+    def get_query_parts
+      query_parts = false
+      OPERATOR_MAP.each do |key, value|
+        if @clause.downcase.include? key
+          query_parts = @clause.downcase.gsub(key, value)
+          break
+        end
+      end
+      return false unless query_parts
+      query_parts.split(' ')
+    end
 
-  	def parse_query_value
-  		value = @query_parts
+    def parse_query_value
+      value = @query_parts
       if is_i?(value)
         value[2].to_i
       elsif is_f?(value)
@@ -61,16 +61,16 @@ module Montage
       elsif @query_operator[0] == 'not in' || @query_operator[0] == 'in'
         value[2].gsub(/('|\(|\))/, "").split(',').map!{ |x| (is_i?(x) ? x.to_i : x) }
       elsif @query_operator[0] == '='
-      	if is_i?(value[1]) 
-      		value[1].to_i
+        if is_i?(value[1]) 
+          value[1].to_i
         else 
-        	value[1].gsub(/('|\(|\))/, "")
+          value[1].gsub(/('|\(|\))/, "")
         end
       else
-      	if is_i?(value[2])
-      		value[2].to_i
+        if is_i?(value[2])
+          value[2].to_i
         else 
-        	value[2].gsub(/('|\(|\))/, "")
+          value[2].gsub(/('|\(|\))/, "")
         end
       end
     end
@@ -91,9 +91,9 @@ module Montage
     end
 
 
-  	def parse_string_clause
+    def parse_string_clause
 
-			raise QueryError, "Your query has an undetermined error" unless @query_parts
+      raise QueryError, "Your query has an undetermined error" unless @query_parts
       raise QueryError, "The operator you have used is not a valid Montage query operator" unless @query_operator[0]
 
       value = parse_query_value
