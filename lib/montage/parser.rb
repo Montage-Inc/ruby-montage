bb3eb0321d4fd5272edf6fc138679c413e2c42b3
Zach <zackify@gmail.com>
2015-04-15 11:11:23 -0700

    breaking out query parsing into seperate methods #7

--- lib/montage/parser.rb
+++ lib/montage/parser.rb
@@ -17,19 +17,63 @@ module Montage
 
   	def initialize(clause=nil)
   		if(clause)
-   			@query = parse_string_clause(clause)
+  			@clause = clause
+   			@column_name = get_column_name
+   			@query_operator = get_query_operator
+   			@montage_operator = get_montage_operator
+   			@query_parts = get_query_parts
+
+   			@query = parse_string_clause
    		end
   	end
 
-  	def parse_query_value(queryparts, operator)
-  		value = queryparts.split(' ')
+  	def get_column_name
+  		@clause.downcase.split(' ')[0]
+  	end
+
+  	def get_query_operator
+  		query_operator = ''
+  		OPERATOR_MAP.each do |key, value|
+			  if @clause.downcase.include? key
+			  	query_operator = key
+			  	break
+			  end
+			end
+			query_operator
+  	end
+
+  	def get_montage_operator
+  		montage_operator = ''
+  		OPERATOR_MAP.each do |key, value|
+			  if @clause.downcase.include? key
+			  	montage_operator = value
+			  	break
+			  end
+			end
+			montage_operator
+  	end
+
+  	def get_query_parts
+  		query_parts = false
+  		OPERATOR_MAP.each do |key, value|
+			  if @clause.downcase.include? key
+			  	query_parts = @clause.downcase.gsub(key, value)
+			  	break
+			  end
+			end
+			return false unless query_parts
+			query_parts.split(' ')
+  	end
+
+  	def parse_query_value
+  		value = @query_parts
       if is_i?(value)
         value[2].to_i
       elsif is_f?(value)
         value[2].to_f
-      elsif operator == 'not in' || operator == 'in'
+      elsif @query_operator == 'not in' || @query_operator == 'in'
         value[2].gsub(/('|\(|\))/, "").split(',').map!{ |x| (is_i?(x) ? x.to_i : x) }
-      elsif operator == '='
+      elsif @query_operator == '='
       	if is_i?(value[1]) 
       		value[1].to_i
         else 
@@ -50,7 +94,7 @@ module Montage
       elsif is_f?(value)
         value.to_f
       else
-        if operator == 'in' || operator == 'not in'
+        if @query_operator == 'in' || @query_operator == 'not in'
           values = value.gsub(/('|\(|\))/, "").split(',').map!{ |x| (is_i?(x) ? x.to_i : x) }
         else
           value.gsub(/('|\(|\))/, "")
@@ -59,23 +103,15 @@ module Montage
       end
     end
 
-  	def parse_string_clause(clause)
 
-  		OPERATOR_MAP.each do |key, value|
-			  if clause.downcase.include? key
-			  	@queryparts = clause.downcase.gsub(key, value)
-			  	@operator = key
-			  	@sqloperator = value
-			  	break
-			  end
-			end
+  	def parse_string_clause
 
-			raise QueryError, "Your query has an undetermined error" unless @queryparts
-      raise QueryError, "The operator you have used is not a valid Montage query operator" unless @sqloperator
+			raise QueryError, "Your query has an undetermined error" unless @query_parts
+      raise QueryError, "The operator you have used is not a valid Montage query operator" unless @montage_operator
 
-      value = parse_query_value(@queryparts,@operator)
+      value = parse_query_value
 
-      { "#{@queryparts.split(' ')[0]}#{@sqloperator}".to_sym => value }
+      { "#{@query_parts[0]}#{@montage_operator}".to_sym => value }
     end
 
     def is_i?(value)
@@ -88,6 +124,6 @@ module Montage
     def is_f?(value)
       /\A\d+\.\d+\z/ === value
     end
-    attr_reader :query
+    attr_reader :query, :query_operator, :montage_operator, :column_name, :clause
   end
 end
