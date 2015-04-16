f84e033f2262f11428c944372771ecfa604ba7ca
dphaener <dphaener@gmail.com>
2015-04-16 10:02:08 -0700

    Move simplecov to top of test helper to swoop up app files'

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,14 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'montage'
-
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'shoulda-context'
-require 'mocha/setup'
-require 'faraday'
 require 'simplecov'
-
-Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new(:color => true)]
 
 # To run tests with coverage:
 # COVERAGE=true rake test
@@ -17,3 +7,15 @@ if ENV['COVERAGE']
   SimpleCov.start do
   end
 end
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'montage'
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'shoulda-context'
+require 'mocha/setup'
+require 'faraday'
+
+
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new(:color => true)]
